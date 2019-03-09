10ff79caefa5f3a8c11148d3ffce9e277382805e
Aeonni <admin@aeonni.com>
2019-03-09 15:47:18 +0800

    some changes

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
-file = './data/每日时间分配-详记.xlsx'
+# Your excel path
+excel_file = './data/每日时间分配-详记.xlsx'
+# Server port and IP address
 port = 1998
-server_ip = '127.0.0.1'
+server_ip = '127.0.0.1' # set '0.0.0.0' if you need access from other networks
+# If reverse the order of timeline chart
+time_ascending = False
